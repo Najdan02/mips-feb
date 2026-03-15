830d76623c1b7de28d56c0e04a7a2193b4925373
Najdan02 <najdan.milosavljevic@gmail.com>
2026-03-15 17:10:50 +0100

    Semantic update to code structure

--- cubemx/code/Core/Src/homework.c
+++ cubemx/code/Core/Src/homework.c
@@ -20,8 +20,8 @@
 #include "driver_motor.h"
 #include "driver_temp.h"
 
-#include "gpio.h"
 #include "keypad.h"
+#include "gpio.h"
 
 TimerHandle_t ledTimer;
 
@@ -31,21 +31,20 @@ static void homeworkTask(void *parameters)
 }
 
 void ledCounter(TimerHandle_t xTimer) {
-    UNUSED(xTimer);
-    if (TEMP_GetCurrentValue() < temp_granica) {
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_13, GPIO_PIN_RESET);
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_SET);
-    } else {
-    	HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_13);
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_RESET);
-    }
+	UNUSED(xTimer);
+	if(TEMP_GetCurrentValue() < temp_granica){
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_13, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_SET);
+	} else {
+		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_13);
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_RESET);
+	}
 }
-
 
 void homeworkInit()
 {
 	TEMP_Init();
-	ledTimer = xTimerCreate("ledTimer", pdMS_TO_TICKS(500), pdTRUE, NULL, ledCounter);
+	ledTimer = xTimerCreate ("ledTimer", pdMS_TO_TICKS(500), pdTRUE, NULL, ledCounter);
 	xTimerStart(ledTimer, portMAX_DELAY);
 }
 
